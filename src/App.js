52ccb003bea873125e1c0e0170fa459b051a876b
Anastasia <anastasia.chashch@mail.ru>
2023-11-17 21:22:03 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./main.css";
+import "./components/Menu/menu.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu/Menu";
+import HomePage from "./components/Pages/HomePage";
+import DriftPage from "./components/Pages/DriftPage";
+import TimeAttackPage from "./components/Pages/TimeAttackPage";
+import ForzaPage from "./components/Pages/ForzaPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route exact path="/ra-router-menu" element={<HomePage />} />
+            <Route path="/ra-router-menu/drift" element={<DriftPage />} />
+            <Route path="/ra-router-menu/timeattack" element={<TimeAttackPage />}/>
+            <Route path="/ra-router-menu/forza" element={<ForzaPage />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+export const fakeDataMenu = [
+    {name:"Главная", path: "/"},
+    {name:"Дрифт-такси", path: "/drift"},
+    {name:"Timeattack", path: "/timeattack"},
+    {name:"Forza Carting", path: "/forza"},
+]
